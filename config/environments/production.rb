b0e0f8b448efaedbb6574028e10b03ba77d46a73
ndviet <ndviet1001@gmail.com>
2020-09-25 12:59:29 +0700

    Adjust for self-hosted.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@
 
 Discourse::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  require 'uglifier'
 
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -14,7 +15,8 @@ Discourse::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.public_file_server.enabled = GlobalSetting.serve_static_assets || false
 
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # stuff should be pre-compiled
   config.assets.compile = false
