98a714aaa88855946df8937f4e42542459aafaff
ndviet <ndviet1001@gmail.com>
2020-09-28 13:39:14 +0700

    Revert production configuration as default.

--- config/puma.rb
+++ config/puma.rb
@@ -7,8 +7,8 @@ if ENV['RAILS_ENV'] == 'production'
   num_workers = ENV["NUM_WEBS"].to_i > 0 ? ENV["NUM_WEBS"].to_i : 4
 
   # Second, you can choose how many threads that you are going to run at same time.
-  workers 2
-  threads 4, 16
+  workers "#{num_workers}"
+  threads 8, 32
 
   # Unless you know what you are changing, do not change them.
   bind "unix://#{APP_ROOT}/tmp/sockets/puma.sock"
